70eb662469e39a294e1502fac59cf29cf34720d1
Maykon L Fonseca <maykonlfonseca@gmail.com>
2020-10-07 20:55:41 -0300

    add login route push

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,12 +1,13 @@
 <template>
   <div>
-    <button v-on:click="loginWithGoogle()">Login With Google</button>
+    <v-btn v-on:click="loginWithGoogle()">Login With Google</v-btn>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import firebase from 'firebase/app'
+import router from '../router/index'
 
 @Component({
   components: {}
@@ -16,6 +17,11 @@ export default class Login extends Vue {
     const provider = new firebase.auth.GoogleAuthProvider()
     firebase.auth().signInWithPopup(provider).then((result) => {
       console.log(result)
+      firebase.auth().currentUser.getIdToken(false).then((token) => {
+        console.log('token', token)
+      })
+
+      router.push('/')
     }).catch((error) => {
       console.error(error)
     })
